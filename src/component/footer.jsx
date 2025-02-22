0d0e32e6acebb6f05c28c9453f9feca6875de169
Richard <esanrichard1@gmail.com>
2025-02-22 16:56:33 +0100

    minor fixes

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="bg-neutral-950 text-gray-400 font-semibold py-3 mt-4 ">
+    <div className="bg-neutral-950 text-gray-400 font-semibold py-3 mt-4 max-sm:text-xs ">
   <div className="text-center">
     <p>&copy; {new Date().getFullYear()} Bolu. All rights reserved.</p>
   </div>
